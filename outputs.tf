b56b3c63455585b6b4e4852ec87f0496cc120b8f
Marc Seiler <mseiler@gmail.com>
2018-02-15 17:40:30 -0500

    added some outputs

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,11 @@
+output "bucket_arn" {
+  value = "${aws_s3_bucket.bucket.arn}"
+}
 
+output "cloudfront_id" {
+  value = "${aws_cloudfront_distribution.s3_distribution.id}"
+}
+
+output "cloudfront_domain" {
+  value = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
+}
