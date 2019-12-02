7f10b9f6f3778d99047ff2dcce79b6e18685c729
Marc Seiler <mseiler@gmail.com>
2019-12-01 21:06:26 -0500

    added bucket_name

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "bucket_arn" {
   value = aws_s3_bucket.bucket.arn
 }
 
+output "bucket_name" {
+  value = aws_s3_bucket.bucket.id
+}
+
 output "cloudfront_id" {
   value = aws_cloudfront_distribution.s3_distribution.id
 }
